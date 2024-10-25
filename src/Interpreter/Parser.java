503dd487451abb8a2951a9405f950b2f9d2c9dbb
Leo Paggen <lpaggen@gmail.com>
2024-10-25 23:04:24 +0200

    fixed function handler -> functions can be parsed into AST nodes through the 'functionNode' ASTNode class

--- src/Interpreter/Parser.java
+++ src/Interpreter/Parser.java
@@ -2,59 +2,52 @@ package Interpreter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 // huge bug fix needed, parsing in parentheses is very hard
 
 public class Parser {
 
     private final List<Token> tokens;
+    // public final ASTNode out;
     private int pos = 0;
 
     public Parser(List<Token> tokens) {
         this.tokens = tokens;
     }
 
+    // this is inherently wrong, should be a while loop until done really...
+    // or something similar in purpose, parsing each line separately until I throw in loops
     public String parseCode(List<Token> tokens) {
-        ASTNode out = parseExpression(tokens, pos);
+        while (pos < tokens.size()) {
+            Token token = tokens.get(pos);
+            if (token.getKind() == TokenKind.SIN || token.getKind() == TokenKind.COS || token.getKind() == TokenKind.TAN || token.getKind() == TokenKind.COT ||
+                    token.getKind() == TokenKind.CSC || token.getKind() == TokenKind.EXP || token.getKind() == TokenKind.LOG) {
+                FunctionNode functionNode = functionHandler(tokens);
+            }
+        }
+
+        ASTNode out = parseExpression(tokens);
         return out.toString();
     }
 
-    // this one is just a helper method to get everything inside a parenthesis
-    private ASTNode parseParenContent() {
-        List<Token> tokensInParen = new ArrayList<>();
-        Token token = tokens.get(pos);
-        while (token.getKind() != TokenKind.CLOSE_PAREN && pos < tokens.size()) { // go over all tokens until encounter )
-            if (token.getKind() != TokenKind.WHITESPACE) {
-                tokensInParen.add(token);
-                pos++;
-                token = tokens.get(pos);
-            }
-        }
-        pos++; // consume )
-        return parseExpression(tokensInParen, 0);
-    }
-
-    private ASTNode parseExpression(List<Token> tokens, int pos) { // this name is bad
-        System.out.println(tokens);
-        Token token = tokens.get(pos);
-        if (token.getKind() == TokenKind.SIN || token.getKind() == TokenKind.COS || token.getKind() == TokenKind.TAN || token.getKind() == TokenKind.COT ||
-            token.getKind() == TokenKind.CSC || token.getKind() == TokenKind.EXP || token.getKind() == TokenKind.LOG) {
-            pos++;
-            token = tokens.get(pos); // not sure if correct... lost track of what is going on
+    private FunctionNode functionHandler(List<Token> tokens) {
+        int pos = 0;
+        while (pos < tokens.size()) {
+            Token token = tokens.get(pos);
             String functionName = token.getValue();
-            if (token.getKind() == TokenKind.OPEN_PAREN) {
-                pos++; // consume (
-                token = tokens.get(pos);
-                ASTNode argument = parseParenContent(); // this is a helper method, seems complex but really isn't
-                return new FunctionNode(functionName, argument); // create the AST node for a function
-            } else {
+            if (tokens.get(pos++).getKind() != TokenKind.OPEN_PAREN) { // check if parenthesis opens, eg cos(x) != cosx
                 throw new RuntimeException("Expected open parenthesis after function declaration");
             }
-        } else if (token.getKind() == TokenKind.INTEGER_TYPE || token.getKind() == TokenKind.FLOAT_TYPE ||token.getKind() == TokenKind.VARIABLE) { // handle other tokens maybe
-            pos++;
-            token = tokens.get(pos);
-            return new VariableNode(token.getValue(), token.getKind().toString()); // might want to rename to something else, we will see
+            List<Token> inParenTokens = new ArrayList<>();
+            while (tokens.get(pos).getKind() != TokenKind.CLOSE_PAREN) {
+                token = tokens.get(pos++);
+                inParenTokens.add(token);
+            }
+            pos++; // consume CLOSE_PAREN
+            ASTNode inParenNodes = functionHandler(inParenTokens);
+            return new FunctionNode(functionName, inParenNodes);
         }
-        throw new RuntimeException("Unexpected token: " + token.getKind());
+        return null;
     }
 }
