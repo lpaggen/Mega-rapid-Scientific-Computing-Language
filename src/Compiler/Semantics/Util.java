85c3fcc1e0293b2c87f1a90110a065e647303962
Leo Paggen <lpaggen@gmail.com>
2024-11-29 12:01:27 +0100

    test commit on util file

--- src/Compiler/Semantics/Util.java
+++ src/Compiler/Semantics/Util.java
@@ -5,6 +5,7 @@ import Compiler.Tokenizer.Token;
 
 // util class (rename later) handles all the transformation from generic nodes to their proper AST nodes
 // so a binary node can become a sum or negation etc
+// we should work on this at some stage.
 public class Util { // have to see how to format all this
     private ASTNode left;
     private ASTNode right;
